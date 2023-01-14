55f4e29bc2a50c9f83291e173772f3bdf4e72b50
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-14 22:30:46 +0100

    feat: add projects

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
+const dataEl = document.querySelector("footer .date")
+dataEl.innerHTML = `${new Date().getFullYear()}`
 
-  // pegar a tag img
-  // const img = document.querySelector("#profile img")
-
-  // substituir a imagem
-  // if (html.classList.contains("light")) {
-  //   // se tiver light mode, adicionar a imagem light
-  //   img.setAttribute("src", "./assets/avatar-light.png")
-  // } else {
-  //   // set tiver sem light mode, manter a imagem normal
-  //   img.setAttribute("src", "./assets/avatar.png")
-  // }
-}
+const switchThemeEl = document.querySelector("#switch-theme")
+switchThemeEl.addEventListener("click", () => {
+  document.documentElement.classList.toggle("light")
+})
